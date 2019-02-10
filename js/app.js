3c0fc61528ab881e0a0910ca2fb1d5b90a4cb21f
Willian <wiolpi07@gmail.com>
2019-02-10 04:00:47 -0200

    Commit de madruga

--- js/app.js
+++ js/app.js
@@ -15,4 +15,62 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  //budge forme
+
+  submmitBudgetForm(){
+    console.log("Chegou"); // aviso q esta funcionando
+    const value = this.budgetInput.value; // pegando o valor passado no html
+    if( value === '' || value < 0 ){ // verificar se n passou valor vazio o negt
+      this.budgetFeedback.classList.add("showItem"); // adicionar o aviso
+      this.budgetFeedback.innerHTML = `<p>O valor esta incorreto</p>` // aviso
+      const self = this; // acessar no escopo settimeout
+      setTimeout(function(){
+        self.budgetFeedback.classList.remove("showItem");
+      },4000) // some 4 seg
+    }else{
+      this.budgetAmount.textContent = value; // salvar a variavel
+      this.budgetInput = ''; // apagar
+      this.showBalance(); 
+    }
+  }
+
 }
+
+
+
+  function eventListeners() {
+    
+    
+    const budget = document.getElementById("budget-form");
+    const expense = document.getElementById("expense-form");
+    const expLista = document.getElementById("expense-list");
+
+    //new instance of UI
+    const ui =  new  UI();
+
+    //budget form submit
+    budget.addEventListener("submit",function(event){
+      event.preventDefault(); // previnir de reexecutar
+      ui.submmitBudgetForm();
+    });
+
+    //expense form submit
+    expense.addEventListener("submit",function(event){
+      event.preventDefault(); // previnir de reexecutar
+
+    });
+
+    //expenseList form submit
+    budget.addEventListener("click",function(event){    
+    });
+
+  
+  } 
+    
+  document.addEventListener('DOMContentLoaded',function(){
+    eventListeners(); 
+  })
+
+
+
